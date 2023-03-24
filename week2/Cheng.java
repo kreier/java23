6e64b91bdbf484bb442f51825d19c7e947f471dd
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-03-24 17:28:39 +0700

    examples from the first session (week2)

---
+++ week2/Cheng.java
@@ -0,0 +1,15 @@
+public class Cheng {
+    public static void main(String[] args)
+    {
+        String myName;
+        myName = "Mr Cheng";
+        String greeting;
+        greeting = "Hello";
+        Integer age;
+        age = 42;
+        String city = "Saigon";
+        String output = "I am " + myName + " and I live in " + city;
+        output += ".\nI am " + age + " years old.";
+        System.out.println(output);
+    }
+}
